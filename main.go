227d29fd1eef17842efc6cf5e9c2ae4e2c7bd85d
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-03-05 11:25:10 +0100

    Removed old code and using the new stuff

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"os"
 
 	"github.com/kr/pretty"
 	"github.com/nanoteck137/pyrin/gen"
@@ -12,146 +10,14 @@ import (
 	"github.com/nanoteck137/pyrin/resolve"
 )
 
-type Status int
-
-const (
-	StatusUnfinished Status = iota
-	StatusProcessing
-	StatusFinished
-)
-
-// type StructDecl struct {
-// 	Name   string
-// 	Status Status
-// 	Fields []FieldDef
-// }
-//
-// type Generator struct {
-// 	Structs map[string]StructDecl
-// }
-//
-// func (gen *Generator) GetType(w io.Writer, fieldType string) (string, error) {
-// 	switch fieldType {
-// 	case "string", "str":
-// 		return "string", nil
-// 	case "int", "number":
-// 		return "int", nil
-// 	default:
-// 		splits := strings.Split(fieldType, " ")
-// 		if len(splits) == 2 {
-// 			if splits[0] == "array" {
-// 				elementTy, err := gen.GenerateStruct(w, splits[1])
-// 				if err != nil {
-// 					return "", err
-// 				}
-//
-// 				return "[]" + elementTy, nil
-// 			}
-// 		}
-// 		return "", fmt.Errorf("Unknown field type '%s'", fieldType)
-// 	}
-// }
-//
-// func (gen *Generator) GenerateField(w io.Writer, field FieldDef) error {
-// 	ty, err := gen.GetType(w, field.Typ)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	fmt.Fprintf(w, "\t%s %s\n", field.Name, ty)
-//
-// 	return nil
-// }
-//
-// func (gen *Generator) GenerateStruct(w io.Writer, name string) (string, error) {
-// 	decl, exists := gen.Structs[name]
-// 	if !exists {
-// 		return "", fmt.Errorf("'%s' don't exist", name)
-// 	}
-//
-// 	if decl.Status == StatusProcessing {
-// 		return "", fmt.Errorf("'%s' cyclic dependency", name)
-// 	}
-//
-// 	if decl.Status == StatusFinished {
-// 		return decl.Name, nil
-// 	}
-//
-// 	decl.Status = StatusProcessing
-//
-// 	fmt.Fprintf(w, "type %s struct {\n", decl.Name)
-// 	for _, f := range decl.Fields {
-// 		err := gen.GenerateField(w, f)
-// 		if err != nil {
-// 			return "", err
-// 		}
-// 	}
-// 	fmt.Fprintf(w, "}\n")
-//
-// 	decl.Status = StatusFinished
-//
-// 	return decl.Name, nil
-// }
-//
-// func Generate(config Config) error {
-// 	var b strings.Builder
-//
-// 	gen := Generator{
-// 		Structs: map[string]StructDecl{},
-// 	}
-//
-// 	for _, s := range config.Structs {
-// 		gen.Structs[s.Name] = StructDecl{
-// 			Name:   s.Name,
-// 			Status: StatusUnfinished,
-// 			Fields: s.Fields,
-// 		}
-// 	}
-//
-// 	pretty.Println(gen)
-//
-// 	for _, s := range config.Structs {
-// 		_, err := gen.GenerateStruct(&b, s.Name)
-// 		if err != nil {
-// 			return err
-// 		}
-// 	}
-//
-// 	fmt.Println(b.String())
-//
-// 	return nil
-// }
-
-type FieldDef struct {
-	Name string `json:"name"`
-	Typ  string `json:"type"`
-}
-
-type StructDef struct {
-	Name   string     `json:"name"`
-	Fields []FieldDef `json:"fields"`
-}
-
-type Config struct {
-	Structs []StructDef `json:"structs"`
-}
-
 func main() {
 	stack := gen.GeneratorStack{}
-	stack.AddGenerator(&gogen.GoGenerator{})
-	stack.AddGenerator(&jsgen.JsGenerator{})
-
-	data, err := os.ReadFile("./test.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var config Config
-	err = json.Unmarshal(data, &config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pretty.Println(config)
+	stack.AddGenerator(gogen.New(gogen.Options{
+		Output:      "./work/types/types.go",
+	}))
+	stack.AddGenerator(jsgen.New(jsgen.Options{
+		Output: "./work/types.js",
+	}))
 
 	resolver := resolve.New()
 	resolver.Structs["ApiGetArtists"] = &resolve.Struct{
@@ -198,16 +64,11 @@ func main() {
 
 	pretty.Println(resolver)
 
-	ty, err := resolver.Resolve("ApiGetArtists")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pretty.Println(ty)
+	resolver.ResolveAll()
 
 	pretty.Println(resolver.ResolvedStructs)
 
-	err = stack.Generate(resolver)
+	err := stack.Generate(resolver)
 	if err != nil {
 		log.Fatal(err)
 	}
