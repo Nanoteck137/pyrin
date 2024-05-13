9bb5c96f9151651a7cb025e827b906749d0d24d5
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-05-13 23:01:15 +0200

    Added zod cmd and removed other cmds

--- cmd/gen.go
+++ cmd/gen.go
@@ -14,133 +14,39 @@ var genCmd = &cobra.Command{
 	Use: "gen",
 }
 
-var genTestCmd = &cobra.Command{
-	Use:  "test <GO_FILE>",
+var genZodCmd = &cobra.Command{
+	Use:  "zod <GO_FILE>",
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		input := args[0]
 
-		// pkg, _ := cmd.Flags().GetString("package")
-		// output, _ := cmd.Flags().GetString("output")
-		// formatCode, _ := cmd.Flags().GetBool("format")
+		output, _ := cmd.Flags().GetString("output")
 
 		file, err := os.ReadFile(input)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		decls := parser.Parse(string(file))
 		resolver := resolve.New()
+		decls := parser.Parse(string(file))
 
 		for _, decl := range decls {
 			resolver.AddSymbolDecl(decl)
 		}
 
-		err = resolver.ResolveAll()
-		if err != nil {
-			log.Fatal(err)
-		}
+		resolver.ResolveAll()
 
 		generator := zod.New(zod.Options{
-			Output: "./work/gen.ts",
+			Output: output,
 		})
 
 		generator.Generate(resolver)
-
-		// d, err := json.MarshalIndent(&test.TestStruct2{}, "", "  ")
-		//
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		//
-		// fmt.Printf("string(d): %v\n", string(d))
 	},
 }
 
-// var genGoCmd = &cobra.Command{
-// 	Use:  "go <PYRIN_FILE>",
-// 	Args: cobra.ExactArgs(1),
-// 	Run: func(cmd *cobra.Command, args []string) {
-// 		input := args[0]
-//
-// 		pkg, _ := cmd.Flags().GetString("package")
-// 		output, _ := cmd.Flags().GetString("output")
-// 		formatCode, _ := cmd.Flags().GetBool("format")
-//
-// 		file, err := os.ReadFile(input)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-//
-// 		decls := parser.Parse(file)
-// 		resolver := resolve.New()
-//
-//
-// 		for _, decl := range decls {
-// 			resolver.AddSymbolDecl(decl)
-// 		}
-//
-// 		resolver.ResolveAll()
-//
-// 		generator := gogen.New(gogen.Options{
-// 			PackageName: pkg,
-// 			Output:      output,
-// 		})
-//
-// 		generator.Generate(resolver)
-//
-// 		if formatCode {
-// 			cmd := exec.Command("gofmt", "-w", output)
-// 			err := cmd.Run()
-// 			if err != nil {
-// 				log.Fatal(err)
-// 			}
-// 		}
-// 	},
-// }
-//
-// var genJsCmd = &cobra.Command{
-// 	Use:  "js <PYRIN_FILE>",
-// 	Args: cobra.ExactArgs(1),
-// 	Run: func(cmd *cobra.Command, args []string) {
-// 		input := args[0]
-//
-// 		output, _ := cmd.Flags().GetString("output")
-//
-// 		file, err := os.Open(input)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-//
-// 		parser := parser.New(file)
-// 		resolver := resolve.New()
-//
-// 		decls := parser.Parse()
-//
-// 		for _, decl := range decls {
-// 			resolver.AddSymbolDecl(decl)
-// 		}
-//
-// 		resolver.ResolveAll()
-//
-// 		generator := jsgen.New(jsgen.Options{
-// 			Output: output,
-// 		})
-//
-// 		generator.Generate(resolver)
-// 	},
-// }
-
 func init() {
-	// genGoCmd.Flags().StringP("package", "p", "types", "Name of the package declaration")
-	// genGoCmd.Flags().StringP("output", "o", "./types/types.go", "Output file")
-	// genGoCmd.Flags().BoolP("format", "f", false, "Use 'gofmt' to format output")
-	//
-	// genJsCmd.Flags().StringP("output", "o", "./src/types/types.ts", "Output file")
-
-	// genCmd.AddCommand(genGoCmd)
-	// genCmd.AddCommand(genJsCmd)
-	genCmd.AddCommand(genTestCmd)
+	genZodCmd.Flags().StringP("output", "o", "./src/types/types.ts", "Output file")
+	genCmd.AddCommand(genZodCmd)
 
 	rootCmd.AddCommand(genCmd)
 }
