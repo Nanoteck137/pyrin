30f453a11424e683025c2b6f61d89635ac20c711
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-05-13 15:26:22 +0200

    First test with golangs parser

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"github.com/nanoteck137/pyrin/cmd"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
 )
 
 func main() {
-	cmd.Execute()
+	// cmd.Execute()
+
+	fset := token.NewFileSet()
+
+	src := `
+	package test
+
+	type TestStruct struct {
+		Field1 string;
+	}
+	`
+
+	f, err := parser.ParseFile(fset, "", src, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, d := range f.Decls {
+		switch d := d.(type) {
+		case *ast.GenDecl:
+			for _, spec := range d.Specs {
+				switch spec := spec.(type) {
+				case *ast.TypeSpec:
+					fmt.Printf("spec.Name.Name: %v\n", spec.Name.Name)
+				}
+			}
+			_ = d
+		default:
+		}
+	}
 }
