11ee18b93d5f45fb188aa5d1e415310211db5c9e
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-05-13 21:47:36 +0200

    Porting the pyrin file to go

--- test/test.go
+++ test/test.go
@@ -1,14 +1,76 @@
 package test
 
-type TestStruct struct {
-	Field1 []string
-	Wooh *string
+type Artist struct {
+	Id      string
+	Name    string
+	Picture string
 }
 
-type TestStruct2 struct {
-	TestStruct
+type Album struct {
+	Id       string
+	Name     string
+	CoverArt string
+	ArtistId string
+}
 
-	Field2, Hello []int
+type Track struct {
+	Id                string
+	Number            int
+	Name              string
+	CoverArt          string
+	Duration          int
+	BestQualityFile   string
+	MobileQualityFile string
+	AlbumId           string
+	ArtistId          string
+	AlbumName         string
+	ArtistName        string
+}
+
+type GetArtists struct {
+	Artists []Artist
+}
+
+type GetArtistById struct {
+	Artist
+}
+
+type GetArtistAlbumsById struct {
+	Albums []Album
+}
+
+type GetAlbums struct {
+	Albums []Album
+}
+
+type GetAlbumById struct {
+	Album
+}
+
+type GetAlbumTracksById struct {
+	Tracks []Track
+}
+
+type GetTracks struct {
+	Tracks []Track
+}
+
+type GetTrackById struct {
+	Track
+	ArtistName int
+}
+
+type GetSync struct {
+	IsSyncing bool
+}
+
+type Test struct {
+	Name string
+}
+
+type Test2 struct {
+	Test  *Test
+	Test2 **int
 }
 
 // TODO(patrik): This is not working
