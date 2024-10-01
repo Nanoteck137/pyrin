9fa322e85cdee8f746997f2137dc4150627ed0ae
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-10-01 20:39:01 +0200

    Restructing

---
+++ cmd/pyrin/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/nanoteck137/pyrin/cmd/pyrin/cli"
+)
+
+func main() {
+	cli.Execute()
+}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/nanoteck137/pyrin/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
